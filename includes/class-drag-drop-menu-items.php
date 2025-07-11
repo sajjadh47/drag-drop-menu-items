65efe3d980593d90f0c9bc917ab7ba2b505ee01c
Sajjad Hossain Sagor <sagorh672@gmail.com>
2025-07-12 02:51:36 +0600

    Updated coding styling - no major changes!!

--- includes/class-drag-drop-menu-items.php
+++ includes/class-drag-drop-menu-items.php
@@ -11,8 +11,7 @@
 /**
  * The core plugin class.
  *
- * This is used to define internationalization, admin-specific hooks, and
- * public-facing hooks.
+ * This is used to define admin-specific hooks and public-facing hooks.
  *
  * Also maintains the unique identifier of this plugin as well as the current
  * version of the plugin.
@@ -52,7 +51,7 @@ class Drag_Drop_Menu_Items {
 	 * Define the core functionality of the plugin.
 	 *
 	 * Set the plugin name and the plugin version that can be used throughout the plugin.
-	 * Load the dependencies, define the locale, and set the hooks for the admin area and
+	 * Load the dependencies, set the hooks for the admin area and
 	 * the public-facing side of the site.
 	 *
 	 * @since     2.0.0
@@ -63,7 +62,6 @@ class Drag_Drop_Menu_Items {
 		$this->plugin_name = 'drag-drop-menu-items';
 
 		$this->load_dependencies();
-		$this->set_locale();
 		$this->define_admin_hooks();
 		$this->define_public_hooks();
 	}
@@ -74,7 +72,6 @@ class Drag_Drop_Menu_Items {
 	 * Include the following files that make up the plugin:
 	 *
 	 * - Drag_Drop_Menu_Items_Loader. Orchestrates the hooks of the plugin.
-	 * - Drag_Drop_Menu_Items_i18n.   Defines internationalization functionality.
 	 * - Drag_Drop_Menu_Items_Admin.  Defines all hooks for the admin area.
 	 * - Drag_Drop_Menu_Items_Public. Defines all hooks for the public side of the site.
 	 *
@@ -92,12 +89,6 @@ class Drag_Drop_Menu_Items {
 		require_once DRAG_DROP_MENU_ITEMS_PLUGIN_PATH . 'includes/class-drag-drop-menu-items-loader.php';
 
 		/**
-		 * The class responsible for defining internationalization functionality
-		 * of the plugin.
-		 */
-		require_once DRAG_DROP_MENU_ITEMS_PLUGIN_PATH . 'includes/class-drag-drop-menu-items-i18n.php';
-
-		/**
 		 * The class responsible for defining all actions that occur in the admin area.
 		 */
 		require_once DRAG_DROP_MENU_ITEMS_PLUGIN_PATH . 'admin/class-drag-drop-menu-items-admin.php';
@@ -109,21 +100,6 @@ class Drag_Drop_Menu_Items {
 		require_once DRAG_DROP_MENU_ITEMS_PLUGIN_PATH . 'public/class-drag-drop-menu-items-public.php';
 
 		$this->loader = new Drag_Drop_Menu_Items_Loader();
-	}
-
-	/**
-	 * Define the locale for this plugin for internationalization.
-	 *
-	 * Uses the Drag_Drop_Menu_Items_i18n class in order to set the domain and to register the hook
-	 * with WordPress.
-	 *
-	 * @since     2.0.0
-	 * @access    private
-	 */
-	private function set_locale() {
-		$plugin_i18n = new Drag_Drop_Menu_Items_i18n();
-
-		$this->loader->add_action( 'plugins_loaded', $plugin_i18n, 'load_plugin_textdomain' );
 	}
 
 	/**
@@ -163,8 +139,7 @@ class Drag_Drop_Menu_Items {
 	}
 
 	/**
-	 * The name of the plugin used to uniquely identify it within the context of
-	 * WordPress and to define internationalization functionality.
+	 * The name of the plugin used to uniquely identify it within the context of WordPress.
 	 *
 	 * @since     2.0.0
 	 * @access    public
